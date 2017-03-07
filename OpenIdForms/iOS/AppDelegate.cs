ac4b1bdbd94a430abcac02e8f6b6326e2b200591
Tyler Jolliff <tyler.jolliff@kbslp.com>
2017-03-07 14:05:19 -0600

    working version of iOS

--- OpenIdForms/iOS/AppDelegate.cs
+++ OpenIdForms/iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using OpenId.AppAuth;
 using UIKit;
 
 namespace OpenIdForms.iOS
@@ -10,13 +11,34 @@ namespace OpenIdForms.iOS
 	[Register("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
+		// The authorization flow session which receives the return URL from SFSafariViewController.
+		public IAuthorizationFlowSession CurrentAuthorizationFlow { get; set; }
+
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
 
+			App.Init(new IOSAppSetup());
+
 			LoadApplication(new App());
 
 			return base.FinishedLaunching(app, options);
+		}
+
+		// Handles inbound URLs. Checks if the URL matches the redirect URI for a pending
+		// AppAuth authorization request.
+		public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+		{
+			// Sends the URL to the current authorization flow (if any) which will process it if it relates to
+			// an authorization response.
+			if (CurrentAuthorizationFlow?.ResumeAuthorizationFlow(url) == true)
+			{
+				return true;
+			}
+
+			// Your additional URL handling (if any) goes here.
+
+			return false;
 		}
 	}
 }
---
+++ OpenIdForms/iOS/OpenIdService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using Foundation;
+using OpenId.AppAuth;
+using UIKit;
+
+namespace OpenIdForms.iOS
+{
+	public class OpenIdService : IOpenIdService
+	{
+		// The OIDC issuer from which the configuration will be discovered.
+		public const string kIssuer = @"https://ssotest.kochind.com/";
+
+		// The OAuth client ID.
+		public const string kClientID = "mobilessotest";
+
+		// The OAuth redirect URI for the client kClientID.
+		public const string kRedirectURI = "com.kbsmad.ssotest.mobilessotest://oauth";
+
+		// NSCoding key for the authState property.
+		public static NSString kAppAuthExampleAuthStateKey = (NSString)"authState";
+
+		private AuthState _authState;
+		public AuthState AuthState
+		{
+			get { return _authState; }
+			set
+			{
+				if (_authState != value)
+				{
+					_authState = value;
+				}
+			}
+		}
+
+		public OpenIdService()
+		{
+		}
+
+		public async Task GetAuthorizationCode()
+		{
+			var issuer = new NSUrl(kIssuer);
+			var redirectURI = new NSUrl(kRedirectURI);
+
+			Console.WriteLine($"Fetching configuration for issuer: {issuer}");
+
+			try
+			{
+				// discovers endpoints
+				var configuration = await AuthorizationService.DiscoverServiceConfigurationForIssuerAsync(issuer);
+
+				Console.WriteLine($"Got configuration: {configuration}");
+
+				string codeMethod = @"plain";
+				string code_verifier = AuthorizationRequest.GenerateCodeVerifier();
+				string state = AuthorizationRequest.GenerateState();
+
+				// builds authentication request
+				AuthorizationRequest request = new AuthorizationRequest(configuration, kClientID, null, Scope.OpenId, redirectURI, ResponseType.Code, state, code_verifier, code_verifier, codeMethod, null);
+
+				// performs authentication request
+				var appDelegate = (AppDelegate)UIApplication.SharedApplication.Delegate;
+				Console.WriteLine($"Initiating authorization request with scope: {request.Scope}");
+
+				appDelegate.CurrentAuthorizationFlow = AuthState.PresentAuthorizationRequest(request, UIApplication.SharedApplication.KeyWindow.RootViewController, (authState, error) =>
+				{
+					if (authState != null)
+					{
+						AuthState = authState;
+						Console.WriteLine($"Got authorization tokens. Access token: {authState.LastTokenResponse.AccessToken}");
+					}
+					else {
+						Console.WriteLine($"Authorization error: {error.LocalizedDescription}");
+						AuthState = null;
+					}
+				});
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine($"Error retrieving discovery document: {ex}");
+				AuthState = null;
+			}
+		}
+	}
+}
